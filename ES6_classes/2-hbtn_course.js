bb69f9d67712da3fd9f02b44ad07dcebfb3dabc1
root <karisrr2203@gmail.com>
2023-09-07 18:50:16 +0000

    2 (still): = -> ==; String -> 'string', Number -> 'number', Array -> 'object'; strings in errors: doublequote -> singlequote; no more semicolon after if/else statements; I don't understand why the check for checking constructor types is passing, but not the check for checking the setter types?? the checks are literally the same??? aaaaaaaaaaa

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,40 +1,40 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name != String) {
-      throw new Error("name needs to be a string");
-    } else if (typeof length != Number) {
-      throw new Error("length needs to be a number");
-    } else if (typeof students != Array) {
-      throw new Error("students needs to be an array");
+    if (typeof name !== 'string') {
+      throw new Error('name needs to be a string');
+    } else if (typeof length !== 'number') {
+      throw new Error('length needs to be a number');
+    } else if (typeof students !== 'object') {
+      throw new Error('students needs to be an array');
     } else {
       this._name = name;
       this._length = length;
       this._students = students;
-    };
+    }
   }
 
   set setName(name) {
-    if (typeof name != String) {
-      throw new Error("name needs to be a string");
+    if (typeof name !== 'string') {
+      throw new Error('name needs to be a string');
     } else {
       this._name = name;
-    };
+    }
   }
 
   set setLength(length) {
-    if (typeof length != Number) {
-      throw new Error("length needs to be a number");
+    if (typeof length !== 'number') {
+      throw new Error('length needs to be a number');
     } else {
       this._length = length;
-    };
+    }
   }
 
   set setStudents(students) {
-    if (typeof students != Array) {
-      throw new Error("students needs to be an array");
+    if (typeof students !== 'object') {
+      throw new Error('students needs to be an array');
     } else {
       this._students = students;
-    };
+    }
   }
 
   get getName() {
