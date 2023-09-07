218396cf4738b1743446d0ceb9c9ca3e5b7196d8
root <karisrr2203@gmail.com>
2023-09-07 17:26:53 +0000

    12: changing value of key to Object.keys(employeeList).length didn't work, so now making it a function (only difference now is setting it as a var and then returning the var opposed to simply returning it with no var as a 'middle-man')

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,7 +1,9 @@
 export default function createReportObject(employeesList) {
   const reportList = {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments: Object.keys(employeesList).length,
+    getNumberOfDepartments(employeesList) {
+      Object.keys(employeesList).length;
+    },
   };
 
   return reportList;
