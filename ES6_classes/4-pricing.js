c451f235a9237d6e2fd0b55c548cb38d6096749e
root <karisrr2203@gmail.com>
2023-09-07 19:58:28 +0000

    4: forgot semicolon in import line and returns -> return in displayFullPrice()

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,4 +1,4 @@
-import Currency from './3-currency'
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
@@ -13,7 +13,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    returns `${this._amount} ${this._currency._name} (${this._currency._code})`;
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
   }
 
   static convertPrice(amount, conversionRate) {
