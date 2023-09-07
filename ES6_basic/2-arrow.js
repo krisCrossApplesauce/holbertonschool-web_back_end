c34c54976e416b805e364ff1cf97555972780ca4
root <karisrr2203@gmail.com>
2023-09-07 17:14:33 +0000

    2: it's literally identical (as far as I can tell (and I quadruple checked)) as Caramon, Cason, AND Ben's code so I copied and pasted from Caramon in the hopes that it'll work

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,5 +1,5 @@
 export default function getNeighborhoodsList() {
-  this.sanFrancisoNeighborhoods = ['SOMA', 'Union Square'];
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
