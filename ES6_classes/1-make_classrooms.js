5d6bd65aa6785b87f2219d94f5efffd091255123
root <karisrr2203@gmail.com>
2023-09-07 17:56:12 +0000

    1-make_classrooms.js

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom'
+
+export default function initializeRooms() {
+  const aRoom = new ClassRoom(19);
+  const bRoom = new ClassRoom(20);
+  const cRoom = new ClassRoom(34);
+
+  return [ aRoom, bRoom, cRoom ];
+}
