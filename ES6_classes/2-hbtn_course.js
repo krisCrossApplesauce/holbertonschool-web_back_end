ff07e7aa9b99e66a804645ef726edcefd63e8885
root <karisrr2203@gmail.com>
2023-09-07 18:21:45 +0000

    2: added if statements to the constructor and the set methods to check if the class objects are the correct type, raising (throwing) an error if they aren't

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,20 +1,40 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof name != string) {
+      throw "name needs to be a string";
+    } else if (typeof length != int) {
+      throw "length needs to be a number";
+    } else if (typeof students) {
+      throw "students needs to be an array";
+    } else {
+      this._name = name;
+      this._length = length;
+      this._students = students;
+    };
   }
 
   set setName(name) {
-    this._name = name;
+    if (typeof name != string) {
+      throw "name needs to be a string";
+    } else {
+      this._name = name;
+    };
   }
 
   set setLength(length) {
-    this._length = length;
+    if (typeof length != int) {
+      throw "length needs to be a number";
+    } else {
+      this._length = length;
+    };
   }
 
   set setStudents(students) {
-    this._students = students;
+    if (typeof students) {
+      throw "students needs to be an array";
+    } else {
+      this._students = students;
+    };
   }
 
   get getName() {
