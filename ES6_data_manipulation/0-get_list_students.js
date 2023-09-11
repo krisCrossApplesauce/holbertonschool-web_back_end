fa8f5797fa1df0cefb9ae61929e769837dfae5b7
root <karisrr2203@gmail.com>
2023-09-11 17:46:15 +0000

    0: apparently it also wants the names in doublequotes, which is odd, also, there's two different given files, they're called 0-get_list_students.test.js and 0-get_list_dtudents_typo.test.js, but as far as I can tell, they're identical?? it's so weird???

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -7,9 +7,9 @@ export class Student {
 }
 
 export default function getListStudents() {
-  const student1 = new Student('Guillaume', 1, 'San Francisco');
-  const student2 = new Student('James', 2, 'Columbia');
-  const student5 = new Student('Serena', 5, 'San Francisco');
+  const student1 = new Student("Guillaume", 1, 'San Francisco');
+  const student2 = new Student("James", 2, 'Columbia');
+  const student5 = new Student("Serena", 5, 'San Francisco');
 
   const students = [student1, student2, student5];
   return students;
