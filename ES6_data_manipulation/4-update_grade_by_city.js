c01a66ba3cfef221235f3d3e641b96fdad05a0f8
root <karisrr2203@gmail.com>
2023-09-12 19:36:58 +0000

    4-update_grade_by_city.js: zut alors! sacre bleugit add .

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,7 @@
 export default function updateStudentGradeByCity(studList, city, newGrades) {
-  const studsInCity = studList.filter((s) => s.location === city);
-  for (s of studsInCity) {
-    for (g of newGrades) {
-      if (s.id === g.studentId) {
-        s.grade = g.grade;
-      }
-    }
-    if (!s.grade) {
-      s.grade = 'N/A';
-    }
-  }
+  const studsInCity = studList.filter((s) => s.location === city).map((stud) => {
+    const maybeGrade = newGrade.find((item) => item.studentId === stud);
+    const grade = maybeGrade ? maybeGrade.grade : 'N/A';
+    return (...stud, grade);
+  });
 }
