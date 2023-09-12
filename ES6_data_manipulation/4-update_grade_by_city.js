c9102f77e7bf81cac4ecdda3bec479e30cea0fe1
root <karisrr2203@gmail.com>
2023-09-12 20:01:02 +0000

    4: stud -> stud.id

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function updateStudentGradeByCity(studList, city, newGrades) {
   const studsInCity = studList.filter((s) => s.location === city).map((stud) => {
-    const maybeGrade = newGrades.find((item) => item.studentId === stud);
+    const maybeGrade = newGrades.find((item) => item.studentId === stud.id);
     const grade = maybeGrade ? maybeGrade.grade : 'N/A';
     return { ...stud, grade };
   });
