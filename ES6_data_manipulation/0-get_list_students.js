18dfcafbc8ed12f35c9858133b8db9e8aac26e34
root <karisrr2203@gmail.com>
2023-09-12 17:13:14 +0000

    0: no class, just the dictionary objects :P

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,7 @@
-export class Student {
-  constructor(firstName, id, location) {
-    this._firstName = firstName;
-    this._id = id;
-    this._location = location;
-  }
-}
-
 export default function getListStudents() {
-  const student1 = new Student("Guillaume", 1, 'San Francisco');
-  const student2 = new Student("James", 2, 'Columbia');
-  const student5 = new Student("Serena", 5, 'San Francisco');
+  const student1 = {id: 1, firstName: 'Guillaume', location: 'San Francisco'};
+  const student2 = {id: 2, firstName: 'James', location: 'Columbia'};
+  const student5 = {id: 5, firstName: 'Serena', location: 'San Francisco'};
 
   const students = [student1, student2, student5];
   return students;
