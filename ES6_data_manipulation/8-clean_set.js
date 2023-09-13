8a14fd0957714c419a6b612dea3d95afae14654a
root <karisrr2203@gmail.com>
2023-09-13 21:15:35 +0000

    8: why specify returning a string if you don't want a string to be returned aaaaaaaaaaaaaaa

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,10 @@
 export default function cleanSet(set, startString) {
-  const str = '';
+  if (!startString || startString.length < 1) return '';
   const cleanSet = [...set]
     .filter((x) => x)
     .filter((x) => x.startsWith(startString))
-    .map((x) => x.substring(startString));
-  for (const x of cleanSet) {
-    if (str !== '') {
-      str.append('-');
-    }
-    str.append(x);
-  }
+    .map((x) => x.substring(startString))
+    .join('-');
 
-  return str;
+  return cleanSet;
 }
