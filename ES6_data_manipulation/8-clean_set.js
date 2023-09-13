861194528ff5b9356560d461e6ff3d349f5f454c
root <karisrr2203@gmail.com>
2023-09-13 21:00:48 +0000

    8-clean_set.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  let str = '';
+  const arr = Array.from(set)
+    .filter((x) => x.startsWith(startString))
+    .map((x) => x.substring(startString));
+  for (const x of arr) {
+    if (str !== '') {
+      str.append('-');
+    }
+    str.append(x);
+  }
+}
