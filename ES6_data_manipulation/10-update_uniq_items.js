6d26e1efa3d2b7df648f4e0a394565e1e1f71bfc
root <karisrr2203@gmail.com>
2023-09-14 16:57:44 +0000

    10: no newMap, also fixed error if statement

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,17 +1,13 @@
 export default function updateUniqueItems(map) {
-  if (!Map.isMap(map)) {
+  if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
 
-  const updatedMap = new Map();
-
   map.forEach((key, value) => {
     if (value === 1) {
-      updatedMap.set(key, 100);
-    } else {
-      updatedMap.set(key, value);
+      map.set(key, 100);
     }
   });
 
-  return updatedMap;
+  return map;
 }
